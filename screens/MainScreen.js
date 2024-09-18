b4713021afe347c5843e2427c5cc6854b9025b2c
Burak Demir <burakdemir400@gmail.com>
2024-09-18 16:37:50 +0300

    visual_fixes

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, Dimensions, TouchableOpacity, Image, Text, Modal, Scr
 import MapView, { Marker } from 'react-native-maps';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function MainScreen() {
     // sayfalar arası geçiş
@@ -19,6 +20,7 @@ export default function MainScreen() {
     const [showActiveList, setShowActiveList] = useState(false);
     const [showYolBakim, setShowYolBakim] = useState(false);
     const [showSonDepremler, setShowSonDepremler] = useState(false);
+    const [showCameras, setShowCameras] = useState(false);
 
     // harita ilk ayarı
     const [region, setRegion] = useState({
@@ -27,6 +29,24 @@ export default function MainScreen() {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
     });
+
+    const cameraLocations = [
+        { id: 1, latitude: 41.0082, longitude: 28.9784 }, // Sultanahmet
+        { id: 2, latitude: 41.0151, longitude: 28.9795 }, // Eminönü
+        { id: 3, latitude: 41.0383, longitude: 28.9869 }, // Beşiktaş
+        { id: 4, latitude: 41.0328, longitude: 28.9768 }, // Kabataş
+        { id: 5, latitude: 41.0467, longitude: 28.9973 }, // Şişli
+        { id: 6, latitude: 41.0486, longitude: 29.0043 }, // Mecidiyeköy
+        { id: 7, latitude: 41.0670, longitude: 29.0419 }, // Levent
+        { id: 8, latitude: 41.0928, longitude: 29.0659 }, // Sarıyer
+        { id: 9, latitude: 41.1010, longitude: 29.0232 }, // Maslak
+        { id: 10, latitude: 41.0136, longitude: 28.9468 }, // Karaköy
+        { id: 11, latitude: 40.9905, longitude: 29.0264 }, // Üsküdar
+        { id: 12, latitude: 40.9807, longitude: 29.0836 }, // Kadıköy
+        { id: 13, latitude: 40.9694, longitude: 29.0641 }, // Moda
+        { id: 14, latitude: 40.9862, longitude: 29.1163 }, // Acıbadem
+        { id: 15, latitude: 41.0724, longitude: 29.0088 }, // Baltalimanı
+    ];
 
     // alt menü düzenleme
     const handlePressHarita = () => {
@@ -63,6 +83,10 @@ export default function MainScreen() {
         setShowSonDepremler(!showSonDepremler);
     };
 
+    const handleShowCameras = () => {
+        setShowCameras(!showCameras);
+    };
+
     // haritada yakınlaşma uzaklaşma
     const zoomIn = () => {
         setRegion((prevRegion) => ({
@@ -83,15 +107,15 @@ export default function MainScreen() {
     // sayfa düzeni
     return (
         <View style={styles.container}>
-            
+
             {/* Üst Taraf */}
             <View style={styles.topBar}>
                 <TouchableOpacity style={styles.topMenuButton} onPress={handleLineMenu}>
-                {/* <TouchableOpacity style={styles.topMenuButton}> */}
+                    {/* <TouchableOpacity style={styles.topMenuButton}> */}
                     <Icon name="bars" size={30} color="#000" />
                 </TouchableOpacity>
 
-                <Image source={require('../assets/akomayskucuk.png')} />
+                <Image source={require('../assets/akomayskucuk.png')} style = {{width: 200, height: 70}} />
                 {/* <Text style={styles.topMenuText}> AKOMAYS </Text> */}
 
                 <TouchableOpacity style={styles.profileButton}>
@@ -104,10 +128,16 @@ export default function MainScreen() {
                 style={styles.map}
                 region={region}
             >
-                <Marker
-                    coordinate={{ latitude: 41.0082, longitude: 28.9784 }}
-                    title="Istanbul"
-                />
+                {showCameras && cameraLocations.map(camera => (
+                    <Marker
+                        key={camera.id}
+                        coordinate={{ latitude: camera.latitude, longitude: camera.longitude }}
+                        title={"Kamera"}
+                        description={"Burada bir kamera var"}
+                    >
+                        <Image source={require('../assets/kucukkamera.png')} style= {{width: 25,height: 25}} />
+                    </Marker>
+                ))}
             </MapView>
 
             {/* Menü Modal */}
@@ -139,34 +169,42 @@ export default function MainScreen() {
             {/* Zoom Butonları */}
             <View style={styles.zoomContainer}>
                 <TouchableOpacity style={styles.zoomButton} onPress={zoomIn}>
-                    <Text style={styles.zoomText}>+</Text>
+                    <Image source={require('../assets/arti.png')} style={{ width: 30, height: 30 }} />
                 </TouchableOpacity>
 
                 <TouchableOpacity style={styles.zoomButton} onPress={zoomOut}>
-                    <Text style={styles.zoomText}>-</Text>
+                    <Image source={require('../assets/eksi.png')} style={{ width: 25, height: 30 }} />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.zoomButton}>
+                    <Image source={require('../assets/konum.png')} style={{ width: 30, height: 30 }} />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.zoomButton}>
+                    <Image source={require('../assets/anasayfa.png')} style={{ width: 30, height: 30 }} />
                 </TouchableOpacity>
             </View>
 
             {/* Sol Ana Butonlar */}
             <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.button} onPress={handlePressHarita}>
-                    <Image source={require('../assets/traffic.png')} style={styles.icon} />
-                    <Text style={styles.text}>Trafik Yoğunluğu</Text>
+                <TouchableOpacity style={styles.zoomButton} onPress={handlePressHarita}>
+                    <Image source={require('../assets/traffic.png')} style={{ width: 30, height: 30 }} />
+                    {/* <Text style={styles.text}>Trafik Yoğunluğu</Text> */}
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button} onPress={handlePressHarita}>
-                    <Image source={require('../assets/street-camera.png')} style={styles.icon} />
-                    <Text style={styles.text}>Kamera</Text>
+                <TouchableOpacity style={styles.zoomButton} onPress={handleShowCameras}>
+                    <Image source={require('../assets/kucukkamera.png')} style={{ width: 30, height: 30 }} />
+                    {/* <Text style={styles.text}>Kamera</Text> */}
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button} onPress={handleSonDepremler}>
-                    <Image source={require('../assets/earthquake.png')} style={styles.icon} />
-                    <Text style={styles.text}>Son Depremler</Text>
+                <TouchableOpacity style={styles.zoomButton} onPress={handleSonDepremler}>
+                    <Image source={require('../assets/earthquake.png')} style={{ width: 30, height: 30 }} />
+                    {/* <Text style={styles.text}>Son Depremler</Text> */}
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button} onPress={handlePressHarita}>
-                    <Image source={require('../assets/arvento.png')} style={styles.icon} />
-                    <Text style={styles.text}>Araç Takip</Text>
+                <TouchableOpacity style={styles.zoomButton} onPress={handlePressHarita}>
+                    <Image source={require('../assets/arvento.png')} style={{ width: 30, height: 30 }} />
+                    {/* <Text style={styles.text}>Araç Takip</Text> */}
                 </TouchableOpacity>
             </View>
 
@@ -237,32 +275,25 @@ export default function MainScreen() {
                         <View style={styles.rowContainer}>
                             <TouchableOpacity style={styles.subMenuButton}>
                                 <Text style={styles.text}>İtfaiye</Text>
-                                {/* <Image source={require('../assets/radar.png')} style={styles.icon} /> */}
+                                <Image source={require('../assets/itfaiye-icon.png')} style={styles.icon} />
                             </TouchableOpacity>
                             <TouchableOpacity style={styles.subMenuButton} onPress={handleYolBakim}>
                                 <Text style={styles.text}>Yol Bakım</Text>
-                                {/* <Image source={require('../assets/station.png')} style={styles.icon} /> */}
+                                <Image source={require('../assets/yolbakim.png')} style={styles.icon} />
                             </TouchableOpacity>
                             <TouchableOpacity style={styles.subMenuButton}>
                                 <Text style={styles.text}>İSKİ</Text>
-                                {/* <Image source={require('../assets/sms.png')} style={styles.icon} /> */}
+                                <Image source={require('../assets/iski.jpg')} style={styles.icon} />
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.subMenuButton}>
+                                <Text style={styles.text}>Ulaşım</Text>
+                                <Image source={require('../assets/ulasim.png')} style={styles.icon} />
                             </TouchableOpacity>
                             <TouchableOpacity style={styles.subMenuButton}>
                                 <Text style={styles.text}>İlçe Sınırları</Text>
                                 {/* <Image source={require('../assets/riskli-noktalar.png')} style={styles.icon} /> */}
                             </TouchableOpacity>
-                            <TouchableOpacity style={styles.subMenuButton}>
-                                <Text style={styles.text}>Ulaşım</Text>
-                                {/* <Image source={require('../assets/riskli-noktalar.png')} style={styles.icon} /> */}
-                            </TouchableOpacity>
-                            <TouchableOpacity style={styles.subMenuButton}>
-                                <Text style={styles.text}>Ulaşım</Text>
-                                {/* <Image source={require('../assets/riskli-noktalar.png')} style={styles.icon} /> */}
-                            </TouchableOpacity>
-                            <TouchableOpacity style={styles.subMenuButton}>
-                                <Text style={styles.text}>Ulaşım</Text>
-                                {/* <Image source={require('../assets/riskli-noktalar.png')} style={styles.icon} /> */}
-                            </TouchableOpacity>
+
                         </View>
                     </ScrollView>
                 </View>
@@ -272,7 +303,7 @@ export default function MainScreen() {
             {showActiveList && (
                 <View style={styles.subMenuContainer2}>
                     <TouchableOpacity style={styles.closeButton2} onPress={() => setShowActiveList(false)}>
-                        <Icon name="chevron-down" size={20}/>
+                        <Icon name="chevron-down" size={20} />
                     </TouchableOpacity>
                     <Text style={styles.listTitle}>Aktif Olay Listesi</Text>
                     <ScrollView style={styles.scrollContainer}>
@@ -311,7 +342,7 @@ export default function MainScreen() {
             {showSonDepremler && (
                 <View style={styles.subMenuContainer2}>
                     <TouchableOpacity style={styles.closeButton2} onPress={() => setShowSonDepremler(false)}>
-                        <Icon name="chevron-down" size={20}/>
+                        <Icon name="chevron-down" size={20} />
                     </TouchableOpacity>
                     <Text style={styles.listTitle}>Son Depremler</Text>
                     <ScrollView style={styles.scrollContainer}>
@@ -395,8 +426,8 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         alignItems: 'center',
         justifyContent: 'center',
-        width: (Dimensions.get('window').width - 40) / 5,
-        height: 80,
+        width: (Dimensions.get('window').width - 120) / 5,
+        height: 60,
         elevation: 5, // Android shadow
     },
     button2: {
@@ -455,7 +486,7 @@ const styles = StyleSheet.create({
         padding: 10,
         marginTop: 25,
         backgroundColor: '#f8f8f8',
-        height: 60,
+        height: 80,
     },
     topMenuText: {
         fontWeight: 'bold',
@@ -540,7 +571,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#f0f8ff',
         padding: 10,
     },
-    scrollContainer2 : {
+    scrollContainer2: {
         // flexDirection: 'row',
         padding: 10,
     },
@@ -564,6 +595,6 @@ const styles = StyleSheet.create({
     },
     rowContainer: {
         flexDirection: 'row',
-        flexWrap: 'wrap', 
+        flexWrap: 'wrap',
     },
 });
